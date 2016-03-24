59dab464dc4adb2894d13a20c310bd1a1270a5fb
Aleynikov Timofey <aleynikov-timofey@yandex.ru>
2016-03-24 14:31:55 +0300

    Drawing game field

--- main.cpp
+++ main.cpp
@@ -3,8 +3,26 @@
 #include "ConsoleSettings.h"
 #include "Cells.h"
 
+void DrawField()
+{
+	static const int w = field::Field.Width(),
+		h = field::Field.Height();
+	cells::CellTypeName type;
+	cells::CellParameters par;
+	for (int i = 0; i < h; i++)
+	{
+		cs::CursorPos(0, i);
+		for (int j = 0; j < w; j++)
+		{
+			type = field::Field(j, i);
+			par = cells::GetCellParameters(type);
+			cs::SetTextColor(par.color);
+			std::cout << par.symbol;
+		}
+	}
+}
+
 int main() {
-	cs::CursorPos(10, 10);
-	std::cout << "Hello, world!" << std::endl;
+	DrawField();
 	return 0;
 }
